d817b4d532665d8e8055e022a83f28e00c93c3de
hanjunLee00 <hanjun5559@naver.com>
2025-03-03 23:47:38 +0900

    feat: 버스 도착예정시간 출력기능 추가
    - 임시로 출력만

---
+++ bus_arrive_time.py
@@ -0,0 +1,28 @@
+import requests
+import xml.etree.ElementTree as ET
+from dotenv import load_dotenv
+import os
+
+SERVICE_KEY = os.getenv("SERVICE_KEY")
+
+# API 요청
+url = 'http://apis.data.go.kr/1613000/ArvlInfoInqireService/getSttnAcctoArvlPrearngeInfoList'
+params = {
+    'serviceKey': '',
+    'pageNo': '1',
+    'numOfRows': '10',
+    '_type': 'xml',
+    'cityCode': '37420',
+    'nodeId': 'TSB372000244'
+}
+
+response = requests.get(url, params=params)
+
+# XML 파싱
+root = ET.fromstring(response.content)
+
+# 데이터 추출
+for item in root.findall(".//item"):
+    bus_number = item.find("routeno").text
+    arrival_time = item.find("arrtime").text
+    print(f"버스 번호: {bus_number}, 예상 도착 시간: {int(arrival_time) // 60}분 후")
